5ccb91597cbd4413172146c74583eeed019b0845
satoshi <satoshi.kumano@kii.com>
2016-07-26 19:44:35 +0900

    Adding cases.

--- thing-if-test.ts
+++ thing-if-test.ts
@@ -4,14 +4,29 @@
 import * as ThingIF from 'thing-if-sdk';
 
 describe("Usage Test", function () {
-    it ("Onboardings", function(done) {
-        const app: ThingIF.App = new ThingIF.App("appID", "appKey", "JP");
-        const owner: ThingIF.TypedID = new ThingIF.TypedID(ThingIF.Types.User, "dummyID");
-        const api: ThingIF.ThingIFAPI = new ThingIF.ThingIFAPI(owner, "dummYToken", app);
+    const app: ThingIF.App = new ThingIF.App("appID", "appKey", "JP");
+    const owner: ThingIF.TypedID = new ThingIF.TypedID(ThingIF.Types.User, "dummyID");
+    const api: ThingIF.ThingIFAPI = new ThingIF.ThingIFAPI(owner, "dummYToken", app);
+    it ("Shows how to onboarding", function(done) {
         const onboardReq = new ThingIF.OnboardWithVendorThingIDRequest("vendorThingID", "password");
-        api.onboardWithVendorThingID(onboardReq).then(function (params:any) {
-            
+        api.onboardWithVendorThingID(onboardReq).then((result:ThingIF.OnboardingResult) => {
+            console.log(result);
+            done();
+        }).catch((error:ThingIF.ThingIFError) => {
+            // Handle error here.
+            done(error);
         });
-        
     });
+
+    it ("Shows how to send command.", function(done){
+        const actions = [{"turnPower" : {"power": true}}];
+        const commandReq: ThingIF.PostCommandRequest = new ThingIF.PostCommandRequest("smart-light-v1", 1, actions);
+        api.postNewCommand(commandReq).then((command:ThingIF.Command) => {
+            done();
+        }).catch((error:ThingIF.ThingIFError) => {
+            // Handle error here.
+            done(error);
+        })
+    });
+
 });
